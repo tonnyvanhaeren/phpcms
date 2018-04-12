af9013df1a3a124686f9f19e53865322709795db
tonnyvanhaeren <antonius.vanhaeren.av@gmail.com>
2018-04-12 08:06:53 +0200

    cleaning admin index page

--- includes/navigation.php
+++ includes/navigation.php
@@ -23,10 +23,8 @@
             
             echo "<li><a href='#'>{$cat_title}</a></li>";
           }
-        
-        
         ?>
-
+        <li><a href="admin">Admin</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Services</a></li>
         <li><a href="#">Contact</a></li>
